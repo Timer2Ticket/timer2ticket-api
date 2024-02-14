90c929578eeb8d350cc1724a63f61b953a966dae
Jan Starustka <starujan@fit.cvut.cz>
2024-02-14 17:15:49 +0100

    authentication added

--- src/routes/webhooks.ts
+++ src/routes/webhooks.ts
@@ -5,6 +5,8 @@ import { User } from '../models/user/user';
 import { WebhookEvent } from '../enums/webhookEvents';
 import { WebhookEventObjectType } from '../enums/webhookServiceObjectType';
 import { databaseService } from '../shared/database_service';
+import { coreService } from '../shared/core_service';
+import { ObjectId } from 'mongodb';
 
 
 const router = express.Router({ mergeParams: true });
@@ -31,12 +33,18 @@ if (Constants.isCommercialVersion) {
 }
 
 
-router.post('/jira', async (req, res) => {
-    let body
-    if (req.body)
-        body = req.body
-    console.log(body)
+router.post('/jira/:connectionId', async (req, res) => {
+    //TODO validate sender
+    if (!req.body)
+        return
+    let connectionId
+    try {
+        connectionId = new ObjectId(req.params.connectionId);
+    } catch (err: any) {
+        return
+    }
     //get webhook type
+    const body = req.body
     const event = _getJiraWebhookEvent(body)
     if (event === WebhookEvent.Error) return
     const eventObject = _getJiraEventObjectType(body)
@@ -46,14 +54,29 @@ router.post('/jira', async (req, res) => {
     if (!accountId)
         return
     //get connections (there may be more, I have to work with all of them)
-    const connections = await databaseService.getConnectionByServiceAccountId(accountId)
-    if (!connections)
+    const connection = await databaseService.getConnectionById(connectionId)
+    if (!connection)
         return
     //check subscription todo
-    //tell Core to do something
-    const newObject = _getJiraObject(body, eventObject)
+    let newObject
+    if (event === WebhookEvent.Deleted)
+        newObject = {}
+    else {
+        newObject = _getJiraObject(body, eventObject)
+        if (!newObject)
+            return
+    }
 
-
+    const data = {
+        service: 'Jira',
+        timestamp: body.timestamp,
+        event: event,
+        eventObject: eventObject,
+        accountId: accountId,
+        connection: connection,
+        newObject: newObject
+    }
+    const coreResponse = await coreService.postWebhook(data)
 })
 
 
@@ -76,7 +99,8 @@ function _getJiraEventObjectType(body: any): WebhookEventObjectType {
     let webhookEvent
     if (body.webhookEvent) webhookEvent = body.webhookEvent
     else return WebhookEventObjectType.Error
-    const event = webhookEvent.split(':')[1].split('_')[0]
+    const splitOnColon = webhookEvent.split(':')
+    const event = splitOnColon[splitOnColon.length - 1].split('_')[0]
     if (event === 'issue')
         return WebhookEventObjectType.Issue
     else if (event === 'project')
@@ -93,9 +117,14 @@ function _getJiraUserId(body: any, objType: WebhookEventObjectType): string | nu
             return body.worklog.author.accountId
         else
             return null
-    } else {
+    } else if (objType === WebhookEventObjectType.Issue) {
         if (body.user && body.user.accountId)
             return body.user.accountId
+        else
+            return null
+    } else {
+        if (body.project && body.project.projectLead && body.project.projectLead.accountId)
+            return body.project.projectLead.accountId
         else
             return null
     }
@@ -104,16 +133,40 @@ function _getJiraUserId(body: any, objType: WebhookEventObjectType): string | nu
 function _getJiraObject(body: any, objType: WebhookEventObjectType): any {
     if (objType === WebhookEventObjectType.Worklog) {
         //data for new Time Entry
-        return {}
-    } else { // issue or project
-        //data for ServiceObject
-        return {}
-        // newObject = {
-        //     id: 1, //issue or project ID
-        //     name: 'name',
-        //     type: 'type',
-        //     projectId: 1
-        // }
+        if (body.worklog && body.worklog.issueId && body.worklog.id && body.worklog.started && body.worklog.timeSpentSeconds) {
+            const worklog = body.worklog
+            const durationInMilliseconds = worklog.timeSpentSeconds * 1000
+            const start = new Date(worklog.started)
+            const comment = worklog.comment ? worklog.comment : ''
+            return {
+                id: `${worklog.issueId}_${worklog.id}`,
+                projectId: null,
+                text: comment,
+                start: start,
+                end: new Date(start.getTime() + durationInMilliseconds),
+                durationInMiliseconds: durationInMilliseconds,
+            }
+        } else
+            return null
+    } else if (objType === WebhookEventObjectType.Issue) { // issue
+        //data for Issue ServiceObject
+        if (body.issue && body.issue.id && body.issue.fields && body.issue.fields.summary && body.issue.fields.project && body.issue.fields.project.id)
+            return {
+                id: body.issue.id,
+                name: body.issue.fields.summary,
+                type: objType,
+                projectId: body.issue.fields.project.id,
+            }
+        else return null
+    } else { // project
+        if (body.project && body.project.id && body.project.name)
+            return {
+                id: body.project.id,
+                name: body.project.name,
+                type: objType,
+                projectId: body.project.id,
+            }
+        else return null
     }
 }
 
